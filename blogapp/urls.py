0a45895beb1ca2ba33a26a2743202cc29167f22f
shubham <shubhamk3120@gmail.com>
2022-04-15 17:18:36 +0530

    new app for user authentication

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from users import views as user_views
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
+    path('register/', user_views.register, name='register'),
     path('admin/', admin.site.urls),
 ]
